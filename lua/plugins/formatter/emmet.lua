20fdefd76ecb52d6b9ee1f8a6ae6c46271aff66e
Cristian Sanchez <cristiansanchez.itjo@gmail.com>
2023-05-28 19:18:55 -0400

    add emmet

---
+++ lua/plugins/formatter/emmet.lua
@@ -0,0 +1,1 @@
+vim.g.user_emmet_leader_key = '.,' --The leader key of Emmet is ',' so '.,,' activate emmet
--- lua/plugins/packer.lua
+++ lua/plugins/packer.lua
@@ -73,5 +73,7 @@ return require('packer').startup(function(use)
 		requires = { 'nvim-tree/nvim-web-devicons', opt = true }
 	}
 
+	--------------- Emmet
+	use('mattn/emmet-vim')
 
 end)
